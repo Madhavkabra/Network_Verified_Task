fcef6b2ab4a90a433a7723628a647b5ed50b6496
Madhav Kabra <madhav.kabra1100@gmail.com>
2023-05-20 16:14:13 +0530

    Update pairing modal UI

--- src/components/PairingModal/index.jsx
+++ src/components/PairingModal/index.jsx
@@ -1,41 +1,76 @@
 import React from 'react';
+import { Modal } from '../Modal';
+import styles from './styles.module.css';
+import { Heading } from '../Heading';
+import { Button } from '../Button';
 
-const PairingModal = ({ currentProposal, onAccept, onCancel }) => {
-  const name = currentProposal?.params?.proposer?.metadata?.name;
-  const url = currentProposal?.params?.proposer?.metadata.url;
-  const methods = currentProposal?.params?.requiredNamespaces.eip155.methods;
-  const events = currentProposal?.params?.requiredNamespaces.eip155.events;
-  const chains = currentProposal?.params?.requiredNamespaces.eip155.chains;
-  const icon = currentProposal?.params.proposer.metadata.icons[0];
+const PairingModal = ({ proposal, onAccept, onCancel }) => {
+  if (!proposal) {
+    return;
+  }
+  const name = proposal?.params?.proposer?.metadata?.name;
+  const url = proposal?.params?.proposer?.metadata.url;
+  const methods = proposal?.params?.requiredNamespaces.eip155.methods;
+  const events = proposal?.params?.requiredNamespaces.eip155.events;
+  const chains = proposal?.params?.requiredNamespaces.eip155.chains;
+  const icon = proposal?.params.proposer.metadata.icons[0];
 
   return (
-    <div>
-      <div>
-        <img
-          src={icon}
-          alt={name}
-        />
+    <Modal open>
+      <Heading title='Session Proposal' />
+      <div className={styles.root}>
+        <div className={styles.basicInfoContainer}>
+          <img
+            src={icon}
+            alt={name}
+            className={styles.img}
+          />
+
+          <div className={styles.nameAndUrlContainer}>
+            <label className={styles.name}>{name}</label>
+            <a
+              href={url}
+              target='_blank'
+              className={styles.url}
+              rel='noreferrer'
+            >
+              {url}
+            </a>
+          </div>
+        </div>
+
+        <div>
+          <p className={styles.heading}>Review eip155 permissions</p>
+
+          <div className={styles.reviewContainer}>
+            <label className={styles.heading}>{chains}</label>
+
+            <p className={styles.subHeading}>Methods</p>
+            <label className={styles.descriptionText}>
+              {methods?.map((method) => method).join(', ')}
+            </label>
+
+            <p className={styles.subHeading}>Events</p>
+            <label className={styles.descriptionText}>
+              {events?.map((event) => event).join(', ')}
+            </label>
+          </div>
+        </div>
+
+        <div className={styles.actionContainer}>
+          <Button
+            title='Cancel'
+            onClick={onCancel}
+            variant='error'
+          />
+          <Button
+            title='Approve'
+            onClick={onAccept}
+            variant='success'
+          />
+        </div>
       </div>
-
-      <div>
-        <p>{name}</p>
-        <p>{url}</p>
-        <p>Chains: {chains}</p>
-
-        {methods?.map((method) => (
-          <p key={method}>{method}</p>
-        ))}
-
-        {events?.map((event) => (
-          <p key={event}>{event}</p>
-        ))}
-      </div>
-
-      <div>
-        <button onClick={onAccept}>Accept</button>
-        <button onClick={onCancel}>Cancel</button>
-      </div>
-    </div>
+    </Modal>
   );
 };
 
