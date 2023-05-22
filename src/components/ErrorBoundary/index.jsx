f9cc966602176aa141036b164f0f0b419eaeaea9
Madhav Kabra <madhav.kabra1100@gmail.com>
2023-05-22 17:06:54 +0530

    Add Error boundary

---
+++ src/components/ErrorBoundary/index.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Modal } from '../Modal';
+import { Container } from '../Container';
+import { Heading } from '../Heading';
+import { Button } from '../Button';
+import styles from './styles.module.css';
+
+export default class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: '' };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error) {
+    this.setState({ ...this.state, error });
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <Modal open>
+          <Container>
+            <Heading title='Something went wrong!' />
+
+            <div className={styles.root}>
+              <p>{this.state.error}</p>
+
+              <div>
+                <Button
+                  title='Got it'
+                  onClick={() =>
+                    this.setState({
+                      ...this.state,
+                      hasError: false,
+                    })
+                  }
+                  variant='warning'
+                />
+              </div>
+            </div>
+          </Container>
+        </Modal>
+      );
+    }
+
+    return this.props.children;
+  }
+}
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { WalletContextProvider } from './store/wallet';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <WalletContextProvider>
-    <App />
-  </WalletContextProvider>
+  <ErrorBoundary>
+    <WalletContextProvider>
+      <App />
+    </WalletContextProvider>
+  </ErrorBoundary>
 );
